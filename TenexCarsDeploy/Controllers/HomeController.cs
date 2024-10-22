722eb92972d9d521f6464a443bbefdec512c0042
Floren-teena <florentinaantigha27@gmail.com>
2024-10-22 21:24:28 +0100

    Implemented the endpoints for the Homepage

--- TenexCarsDeploy/Controllers/HomeController.cs
+++ TenexCarsDeploy/Controllers/HomeController.cs
@@ -1,19 +1,40 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TenexCarsDeploy.Data.Models;
+using TenexCarsDeploy.Data.Repositories.Interfaces;
+using TenexCarsDeploy.Interfaces;
 using TenexCarsDeploy.Models;
+using TenexCarsDeploy.Models.ViewModels;
+using TenexCarsDeploy.TenexCarsDeploy.Data.DTOs;
 
 namespace TenexCarsDeploy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IVehicleRepository _vehicleRepository;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+        private readonly ISubscriberRepository _subscriberRepository;
+        private readonly IEmailService _emailService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IVehicleRepository vehicleRepository, UserManager<AppUser> userManager, SignInManager<AppUser> signManager, ISubscriberRepository subscriberRepository, IEmailService emailService)
         {
             _logger = logger;
+            _vehicleRepository = vehicleRepository;
+            _userManager = userManager;
+            _subscriberRepository = subscriberRepository;
+            _emailService = emailService;
+            _signManager = signManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var topUniqueVehicles = await _vehicleRepository.GetTopUniqueVehiclesAsync();
+            return View(topUniqueVehicles);
+        }
+        public IActionResult Services()
         {
             return View();
         }
@@ -22,11 +43,78 @@ namespace TenexCarsDeploy.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Operator()
+        {
+            return View();
+        }
+
+        public IActionResult Subscriber()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactPageViewModel contactPageViewModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(contactPageViewModel);
+                }
+
+                var emailContent = new EmailDto
+                {
+
+                    From = contactPageViewModel.Email,
+                    Subject = contactPageViewModel.Subject,
+                    Body = $"Name: {contactPageViewModel.FullName}\n\n Message:\n{contactPageViewModel.Body}"
+                };
+
+                await _emailService.SendEmailToCompany(emailContent);
+                TempData["success"] = "Email sent successfully!";
+                return View(contactPageViewModel);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "Something went wrong when sending email");
+                TempData["error"] = "Something went wrong when sending email";
+                return View(contactPageViewModel);
+            }
+
+        }
+
+        /*public IActionResult Admin()
+        {
+            return View();
+        }*/
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+        public IActionResult Testimonies()
+        {
+            return View();
         }
     }
 }
